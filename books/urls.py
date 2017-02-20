dfa1e9f7ef839711c8b8bf68e4f71a2d274edbaf
Insung Park <magarage0702@gmail.com>
2017-02-20 22:55:20 +0900

    add book application html template

--- books/urls.py
+++ books/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+
+from books import views
 
 urlpatterns = [
-    url(r'^books/', include('books.urls', namespace='books')),
-    url(r'^polls/', include('polls.urls', namespace='polls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.BooksModelView.as_view(), name='index'),
+
+    url(r'^book/$', views.BookList.as_view(), name='book_list'),
+    url(r'^author/$', views.AuthorList.as_view(), name='author_list'),
+    url(r'^publisher/$', views.PublisherList.as_view(), name='publisher_list'),
+
+    url(r'^book/(?P<pk>\d+)/$', views.BookDetail.as_view(), name='book_detail'),
+    url(r'^author/(?P<pk>\d+)/$', views.AuthorDetail.as_view(), name='author_detail'),
+    url(r'^publisher/(?P<pk>\d+)/$', views.PublisherDetail.as_view(), name='publisher_detail'),
 ]
