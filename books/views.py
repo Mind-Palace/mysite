a003fe328ec901c3686798ef18e30c8f06e68df7
Insung Park <magarage0702@gmail.com>
2017-02-23 23:56:37 +0900

    add class views and finished the book application

--- books/views.py
+++ books/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from django.views.generic import ListView
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+# template view
+from books.models import Book, Author, Publisher
+
+
+class BooksModelView(TemplateView):
+    template_name = 'books/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BooksModelView, self).get_context_data(**kwargs)
+        context['object_list'] = ['Books', 'Author', 'Publisher']
+        return context
+
+
+# list view
+class BookList(ListView):
+    model = Book
+
+
+class AuthorList(ListView):
+    model = Author
+
+
+class PublisherList(ListView):
+    model = Publisher
+
+
+# detail view
+class BookDetail(DetailView):
+    model = Book
+
+
+class AuthorDetail(DetailView):
+    model = Author
+
+
+class PublisherDetail(DetailView):
+    model = Publisher
+
+
